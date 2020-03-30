4ed7b49171000d5646620746d322a239aca03a48
Relman <fullerz999@hotmail.com>
2020-03-30 23:49:06 +0100

    Added Fire trap
    Firetrap initiates a one time fire when lockpicking is tried

--- lua/entities/Alarm_Addon/init.lua
+++ lua/entities/Alarm_Addon/init.lua
@@ -6,7 +6,6 @@ function ENT:Initialize()
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_VPHYSICS )
 	self:SetSolid( SOLID_VPHYSICS )
-	self.isRunning = false
 	local phys = self:GetPhysicsObject()
 	if (phys:IsValid()) then
 		phys:Wake()
